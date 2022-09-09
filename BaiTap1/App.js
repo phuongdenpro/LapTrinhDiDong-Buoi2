52831f5acceada360736ee7fa166793da32e8144
phuong <phuongdenpro@gmail.com>
2022-09-09 13:51:11 +0700

    update

--- BaiTap1/App.js
+++ BaiTap1/App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  TextInput,
+  Button,
+  View,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const [name, setName] = React.useState(null);
+  
+  
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView>
+      <TextInput
+        style={styles.textInput}
+        onSubmitEditing={(text)=>setName(text)}
+        value={name}
+        placeholder="Name" 
+      ></TextInput>
+      <Button
+        style={styles.button}
+        title="Show"
+        color="#f194ff"
+       // onPress={handle}
+      ></Button>
+
       <StatusBar style="auto" />
-    </View>
+      <View styles={styles.view}>
+        <Text style={styles.textShow}>Hello: {name}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  textInput: {
+    marginVertical: 100,
+    marginHorizontal: 50,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 300,
+    height: 50,
+  },
+  button: {},
+  view: {},
+  textShow: {
+    marginVertical: 50,
+    marginHorizontal: 100,
+    fontSize: 25,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
